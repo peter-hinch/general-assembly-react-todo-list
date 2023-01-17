a13aa0d03585115ac0e84334756d13df2bc02b44
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2023-01-17 13:43:46 +1100

    Center app on screen using CSS flex.

--- src/components/MyList/MyList.js
+++ src/components/MyList/MyList.js
@@ -11,23 +11,24 @@ import * as s from './MyList.styles';
 
 // Instead of passing in ALL props, destructuring can be used to pass in only
 // what is needed.
-const MyList = ({todos}) => {
-  // Use a return statement when somthing needs to be declared outside of the 
+const MyList = ({ todos }) => {
+  // Use a return statement when somthing needs to be declared outside of the
   // return statement.
   const [todoItemArray, setTodoItemArray] = useState(todos);
   const [currAddInput, setAddInput] = useState('');
-  
+
   // The useEffect hook can be used to monitor a variable and perform logic
   // when React detects a change.
   // Reference: https://reactjs.org/docs/hooks-effect.html
   useEffect(() => {
     localStorage.setItem('todoList', JSON.stringify(todoItemArray));
-  },[todoItemArray]);
+  }, [todoItemArray]);
 
   // CRUD methods - addItem, updateTask, toggleStatus, deleteItem, deleteAllItems.
   const addItem = (event) => {
     event.preventDefault();
-    setTodoItemArray([...todoItemArray,
+    setTodoItemArray([
+      ...todoItemArray,
       {
         id: generateId(),
         task: currAddInput,
@@ -35,45 +36,50 @@ const MyList = ({todos}) => {
       }
     ]);
     setAddInput('');
-  }
+  };
 
   const updateTask = (event) => {
     event.preventDefault();
     const idToUpdate = parseInt(event.target.parentNode.parentNode.id);
-    const indexToUpdate = todoItemArray.map((item) => (item.id)).indexOf(idToUpdate);
+    const indexToUpdate = todoItemArray
+      .map((item) => item.id)
+      .indexOf(idToUpdate);
     const updatedTodoItemArray = [...todoItemArray];
     updatedTodoItemArray[indexToUpdate].task = event.target.value;
     setTodoItemArray(updatedTodoItemArray);
-  }
-  
+  };
+
   // toggleStatus - Performs updates to state when a checkbox is changed.
   const toggleStatus = (event) => {
     event.preventDefault();
     const idToUpdate = parseInt(event.target.parentNode.id);
-    const indexToUpdate = todoItemArray.map((item) => (item.id)).indexOf(idToUpdate);
+    const indexToUpdate = todoItemArray
+      .map((item) => item.id)
+      .indexOf(idToUpdate);
     const updatedTodoItemArray = [...todoItemArray];
     updatedTodoItemArray[indexToUpdate].done = event.target.checked;
     setTodoItemArray(updatedTodoItemArray);
-  }
-  
+  };
+
   const deleteItem = (event) => {
     event.preventDefault();
     const idToDelete = parseInt(event.target.parentNode.id);
-    setTodoItemArray(todoItemArray.filter((item) => (item.id !== idToDelete)));
-  }
-  
+    setTodoItemArray(todoItemArray.filter((item) => item.id !== idToDelete));
+  };
+
   const deleteAllItems = (event) => {
     event.preventDefault();
     setTodoItemArray([]);
-  }
-  
-  // Iterates through existing todos and generates an id one higher than the 
+  };
+
+  // Iterates through existing todos and generates an id one higher than the
   // largest value found.
   const generateId = () => {
-    return todoItemArray.reduce((highest, index) => (
-      index.id > highest ? index.id + 1 : highest + 1), 0
+    return todoItemArray.reduce(
+      (highest, index) => (index.id > highest ? index.id + 1 : highest + 1),
+      0
     );
-  }
+  };
 
   // Generate a list of ListItem elements for display in the return statement.
   const generateList = todoItemArray.map((item) => (
@@ -87,25 +93,34 @@ const MyList = ({todos}) => {
   ));
 
   return (
-    <s.StyledToDoList>
-      <s.StyledH1>Todo List</s.StyledH1>
-      <s.StyledUl>
-        {generateList}
-      </s.StyledUl>
-      <s.StyledForm>
-        <s.StyledRuledSection>
-          <s.StyledInput type="text"
-            id="input-add"
-            placeholder="New item.."
-            onChange={(event) => setAddInput(event.target.value)}
-            value={currAddInput}
-          />
-          <s.StyledButtonAdd aria-label="Add new item" onClick={addItem}></s.StyledButtonAdd>
-        </s.StyledRuledSection>
-        <s.StyledButtonDeleteAll aria-label="Clear list" onClick={deleteAllItems}>Clear List</s.StyledButtonDeleteAll>
-      </s.StyledForm>
-    </s.StyledToDoList>
-  )
+    <s.StyledListContainer>
+      <s.StyledToDoList>
+        <s.StyledH1>Todo List</s.StyledH1>
+        <s.StyledUl>{generateList}</s.StyledUl>
+        <s.StyledForm>
+          <s.StyledRuledSection>
+            <s.StyledInput
+              type="text"
+              id="input-add"
+              placeholder="New item.."
+              onChange={(event) => setAddInput(event.target.value)}
+              value={currAddInput}
+            />
+            <s.StyledButtonAdd
+              aria-label="Add new item"
+              onClick={addItem}
+            ></s.StyledButtonAdd>
+          </s.StyledRuledSection>
+          <s.StyledButtonDeleteAll
+            aria-label="Clear list"
+            onClick={deleteAllItems}
+          >
+            Clear List
+          </s.StyledButtonDeleteAll>
+        </s.StyledForm>
+      </s.StyledToDoList>
+    </s.StyledListContainer>
+  );
 };
 
 export default MyList;
--- src/components/MyList/MyList.styles.js
+++ src/components/MyList/MyList.styles.js
@@ -3,8 +3,15 @@ import imgBackgroundPaper from './../../images/paper.png';
 import imgIconAdd from './../../images/plus-add.svg';
 import imgButtonEraser from './../../images/eraser.png';
 
-const StyledToDoList = styled.div`
-  margin-top: 3rem;
+export const StyledListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+export const StyledToDoList = styled.div`
   padding: 1.5rem;
   width: 16rem;
   transform: rotate(-1deg);
@@ -12,37 +19,37 @@ const StyledToDoList = styled.div`
   background-size: cover;
 `;
 
-const StyledH1 = styled.h1`
+export const StyledH1 = styled.h1`
   font-family: 'Sriracha', sans-serif;
   text-align: center;
   margin: 0 0 1rem 0;
   opacity: 70%;
 `;
 
-const StyledUl = styled.ul`
+export const StyledUl = styled.ul`
   margin: 0;
   padding-left: 0;
 `;
 
-const StyledForm = styled.form`
+export const StyledForm = styled.form`
   width: 100%;
   display: flex;
   flex-wrap: wrap;
 `;
 
-const StyledRuledSection = styled.div`
+export const StyledRuledSection = styled.div`
   width: 100%;
   display: flex;
   flex-basis: 100%;
   border-bottom: 1px solid gray;
 `;
 
-const StyledInput = styled.input`
+export const StyledInput = styled.input`
   width: 1.2rem;
   height: 1.2rem;
 `;
 
-const StyledButton = styled.button`
+export const StyledButton = styled.button`
   margin-left: 0.3rem;
   width: 1.5rem;
   height: 1.5rem;
@@ -50,12 +57,12 @@ const StyledButton = styled.button`
   border: 0;
 `;
 
-const StyledButtonAdd = styled(StyledButton)`
+export const StyledButtonAdd = styled(StyledButton)`
   background: url(${imgIconAdd}) no-repeat;
   opacity: 70%;
 `;
 
-const StyledButtonDeleteAll = styled(StyledButton)`
+export const StyledButtonDeleteAll = styled(StyledButton)`
   position: relative;
   top: 4rem;
   left: 4rem;
@@ -70,8 +77,3 @@ const StyledButtonDeleteAll = styled(StyledButton)`
   background: url(${imgButtonEraser}) no-repeat;
   background-size: cover;
 `;
-
-export {
-  StyledToDoList, StyledH1, StyledUl, StyledForm, StyledRuledSection, StyledInput,
-  StyledButton, StyledButtonAdd, StyledButtonDeleteAll
-};
